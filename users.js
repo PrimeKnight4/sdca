378914f299331513929070726739d0646c118015
PrimeKnight4 <emperorsubtilizer@gmail.com>
2024-07-27 17:11:37 +0800

    Adding all files

--- users.js
+++ users.js
@@ -1,11 +1,11 @@
 module.exports = [
     { // sita
-      token: "MTIyNzY1NjUyMzY0ODM0MDAxOQ.Gdz2H0.DYlF4FXiKUjC9B7szSOrUK5QOsK-Zx_DlWjx0M",
+      token: "",
       data: require("./data/sita.js"),
     },
     
     { // iry
-      token: "MTIxODg2MDAxMjI0ODYzMzM5NA.Gi5RJh.I7j-og0bxlUst14k7yHFIrGkymVJehmL6WpHyo",
+      token: "",
       data: {
         channels: require("./old_data/iry/channels.json"),
         messages: require("./old_data/iry/messages.js"),
